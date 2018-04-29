8cdf3db8897c42dd84f498e7d536a5cfe305a8dd
Albert <adwwang@me.com>
2018-04-29 13:03:03 +1000

    plot1 complete

--- plot2.R
+++ plot2.R
@@ -1,3 +1,20 @@
 ## Exploratory Data Analysis: Course Project 1
 ## Plot 2
 ## Albert Wang
+
+#note: this script assumes that the current working directory contains the required Electric Power Consumption Data Set (household_power_consumption.txt).
+
+#read in the required data
+powerdataall <- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors = FALSE)
+#subset for only 1/2/2007 and 2/2/2007 as required
+powerdata <- subset(powerdataall, Date == "1/2/2007" | Date =="2/2/2007")
+#remove powerdataall to save space
+rm(powerdataall)
+
+#create plot
+####
+
+#output png file and close the graphics device
+dev.copy(png, file="plot2.png")
+dev.off()
+#note that the output png file will be located in the current working directory
